4f3a855108743752ba33e9ae32feb1a790a6b922
iwatatomomi-cmyk <iwatatm8@gmail.com>
2026-07-29 11:20:50 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+package tresure;
+
+import explorer.*;
+import tresure.*;
+
+public class Main{
+    System.out.println("1：冒険者");
+    System.out.println("2：考古学者");
+    System.out.println("3：ドローン操縦者");
+    System.out.println("4：パイロット");
+    System.out.print("キャラクターを選んでください:");
+    int input = new java.util.Scanner(System.in).nextInt();
+
+    if(input==1){
+	
+    }
+    if(input==2){
+    }
+    if(input==3){
+    }
+    if(input==4){
+    }
+    
+
+
+    
+
