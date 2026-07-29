b7c30f79ccfaa993391f1d726a61f4162fc73f1b
3044shiro-lang <3044shiro@gmail.com>
2026-07-29 11:09:36 +0900

    Update Adventurer.java

--- Adventurer.java
+++ Adventurer.java
@@ -1,6 +1,11 @@
-package explorer;
+package LandExplorer;
 public class Adventurer extends LandExplorer{
     Adventurer(String name){
 	super(name);
     }
+
+    public void rateup(){
+	System.out.println(this.getName()+"はレジェンダリの確率をアップさせた！");
+	System.out.println()   
+	    }
 }
