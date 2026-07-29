e337c6fa19073bb0a06592c80723ced9b2d35f1e
3044shiro-lang <3044shiro@gmail.com>
2026-07-29 11:26:38 +0900

    Update Archaeologist.java

--- Archaeologist.java
+++ Archaeologist.java
@@ -1,4 +1,8 @@
 public class Archaeologist extends LandExplorer{
+	Archaeologist(String name){
+		super(name);
+	}
+	
     public void ScoreUp(Treasure t){
 	System.out.println(getName()+"は発掘して宝を見つけた！");
 	t.Point()+=10;
