e3c304484f4d556da249eb423c4d3e1327f4522e
takahashi-mina-code <mina.3737.tm@gmail.com>
2026-07-29 11:12:14 +0900

    Update Archaeologist.java

--- Archaeologist.java
+++ Archaeologist.java
@@ -1,6 +1,6 @@
 public class Archaeologist extends LandExplorer{
     public void ScoreUp(Treasure t){
-	System.out.println(this.name+"は発掘して宝を見つけた！");
+	System.out.println(getName()+"は発掘して宝を見つけた！");
 	t.Point()+=10;
     }
 }
