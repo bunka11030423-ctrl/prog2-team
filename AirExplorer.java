c37fa093302ba92bc3a71f147626c5f7b2d8de9d
y000000000633 <nizikodaiko1903@gmail.com>
2026-07-29 11:35:26 +0900

    Update AirExplorer.java

--- AirExplorer.java
+++ AirExplorer.java
@@ -1,8 +1,10 @@
 package explorer;
 
 public abstract class AirExplorer extends Explorer{
- 
-    public void searchAir() {
+ 	public AirExplorer(String name) {
+	 super(name);
+	}
+	public void searchAir() {
 	System.out.println(getName()+"は空中を探索した");
     }
 }
