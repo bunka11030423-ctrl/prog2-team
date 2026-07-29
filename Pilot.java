cba09928892faa5f8f0916e0a799299e5781c4a0
iwatatomomi-cmyk <iwatatm8@gmail.com>
2026-07-29 11:32:00 +0900

    Update Pilot.java

--- Pilot.java
+++ Pilot.java
@@ -4,6 +4,9 @@ public class Pilot extends AirExplorer{
     public Pilot(String name){
 	setName(name);
 		public void rateup(){
+			System.out.println(this.getName()+"はレアの確率をアップさせた！");
+	System.out.println()   
+	    }
 			
 		System.out.println(this.getName()"は宝を見つけた！");
     }
