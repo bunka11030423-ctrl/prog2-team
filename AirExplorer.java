c0afda7bcee71b189bb0c7906f2e834fa7f72e30
iwatatomomi-cmyk <iwatatm8@gmail.com>
2026-08-04 22:50:44 +0900

    Update AirExplorer.java

--- AirExplorer.java
+++ AirExplorer.java
@@ -1,11 +1,27 @@
-package explorer;
+package treasure.explorer;
 
-public abstract class AirExplorer extends Explorer{
- 	public AirExplorer(String name) {
-	 super(name);
-	}
-	public void searchAir() {
-	System.out.println(getName()+"は空を探索した");
+import treasure.treasure.Treasure;
+
+public abstract class AirExplorer extends Explorer {
+    private int failRate = 20;
+
+    public AirExplorer() {
+    }
+
+    public void setFailRate(int failRate) {
+        this.failRate = failRate;
+    }
+
+    @Override
+    public void explore(Treasure treasure) {
+        int chance = new java.util.Random().nextInt(100);
+        if (chance < failRate) {
+            System.out.println(getName() + " は上空から探索したが何も見つからなかった！");
+            return;
+        }
+
+        System.out.println(getName() + " は上空から探索した！");
+        addScore(treasure.getValue());
+        System.out.println(treasure.getName() + "を見つけた！");
     }
 }
-    
