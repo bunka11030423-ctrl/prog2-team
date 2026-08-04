f771d3377d031deb51fee25c6b4dfd2c3d72ead5
iwatatomomi-cmyk <iwatatm8@gmail.com>
2026-08-04 22:44:34 +0900

    Update Pilot.java

--- Pilot.java
+++ Pilot.java
@@ -1,13 +1,8 @@
 package treasure.explorer;
 
 public class Pilot extends AirExplorer{
-    public Pilot(String name){
-	super(name);
-		public void rateup(){
-			System.out.println(this.getName()+"はレアの確率をアップさせた！");
-		    System.out.println(this.getName()"は宝を見つけた！");
-
-	System.out.println()   
-	    }
-			
+    public Pilot(){
+	setName("パイロット");
+	setFailRate(0);
     }
+}
